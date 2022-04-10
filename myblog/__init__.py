a82d34a50bd0d96d77f808ee987f81617de92429
Nolusindiso Hleko <melanated_butterfly@Badass-MacBook-Pro.local>
2022-04-10 14:40:29 +0900

    trying to create the database table

--- myblog/__init__.py
+++ myblog/__init__.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-app = Flask(__name__)
+
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myblog.db'
+app.config['SECRET_KEY']='NOSI199one'
 db = SQLAlchemy(app)
-
-from myblog import routes
+bcrypt = Bcrypt(app)
+from .models import User
+from myblog import routes,db
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User}
--- myblog/routes.py
+++ myblog/routes.py
@@ -1,11 +1,38 @@
-from myblog import app,db
-from flask import Flask, render_template
+import bcrypt
+from flask import Flask, flash, render_template, request, redirect, url_for
+from myblog import app,db, Bcrypt
 from .models import User
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods =['GET', 'POST'] )
 def register():
+    if request.method == "POST": #Checks if user and email already exist and flashes a message
+        user=User.query.filter_by(username = request.form.get('username')).first()
+        if user:
+            flash("The username already exists!",'warning')
+            return redirect(url_for('register'))
+        email=User.query.filter_by(email = request.form.get('email')).first()
+        if email:
+            flash("The email is taken!")
+            return redirect(url_for('register','warning'))
+        name= request.form.get("name")
+        username= request.form.get("username")
+        email= request.form.get("email")
+        password= request.form.get("password")
+        repeat_password= request.form.get("repeat_password",'warning')
+        if password != repeat_password:
+            flash('passwords do not match','warning')
+            return redirect(url_for('register'))
+        password_hash =bcrypt.generate_password_hash(password)
+        users = User (name=name, username=username, email=email, password=password_hash)
+        db.ession.add(users)
+        db.session.commit()
+        flash('THhank you for registration', 'success')
+        return redirect (url_for ("dashboard"))
+
+        
     return render_template('admin/register.html')
