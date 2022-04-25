b003268d92e8f43670834f8fb09b20f626444a4b
Nolusindiso Hleko <melanated_butterfly@Badass-MacBook-Pro.local>
2022-04-26 02:07:13 +0900

    database issues again

--- myblog/__init__.py
+++ myblog/__init__.py
@@ -14,7 +14,7 @@ login_manager = LoginManager(app)
 
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Weblog2022.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://iznpzyqrqaiogp:76f9a87a5fbb8a5b9d1d451191b9802ec51a81603bcebe5c598a26f3d4c25bfd@ec2-3-229-252-6.compute-1.amazonaws.com:5432/d6r6q00quh18bi'
 app.config['SECRET_KEY']='NOSI199one'
 db = SQLAlchemy(app)
 from flask_bcrypt import Bcrypt
